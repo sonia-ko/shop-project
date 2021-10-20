1cce51229db1f330ed5de529d0afeea8d26c0f0f
Sonia Kozytska <sonya-ko@outlook.com>
2021-10-20 11:23:56 +0300

    define website structure

---
+++ src/config.tsx
@@ -0,0 +1,13 @@
+// config variables will be stored here.
+
+const productCategories = [
+  "Electronics",
+  "Food",
+  "Clothes",
+  "Skin and care",
+  "Toys",
+  "Special nutrition",
+  "Sports and outdoors",
+  "Books",
+];
+export default productCategories;
