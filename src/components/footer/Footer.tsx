d9f75c8c3e39366d7ee2115da88f82ae5f1b0505
Sonia Kozytska <sonya-ko@outlook.com>
2021-11-10 15:29:31 +0200

    working on pull request

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import classes from "./Footer.module.css";
 import ProductsTag from "./productTags/ProductTags";
 import SocialLinksBlock from "./socialLinksBlock/SocialLinksBlock";
 import CopyRightBlock from "./copyRightBlock/CopyRightBlock";
 
 const Footer: React.FC = () => {
   return (
-    <footer className={classes.container}>
+    <footer>
       <SocialLinksBlock />
       <ProductsTag />
       <CopyRightBlock />
